ebcdea30dc8005e7b225354ffb0fe620a3bad684
小V <2571440661@qq.com>
2017-01-06 21:01:27 +0800

    Init frame

---
+++ app/admin/controller/__init__.py
@@ -0,0 +1,7 @@
+#-*- coding: utf-8 -*-
+'''
+Created on 2017年1月6日
+
+@author: Venus
+'''
+from controller import *
---
+++ app/common/model/indexModel.py
@@ -0,0 +1,18 @@
+#-*- coding: utf-8 -*-
+'''
+Created on 2017年1月6日
+
+@author: Venus
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
